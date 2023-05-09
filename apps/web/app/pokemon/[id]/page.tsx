9a195824eeca9849c9665384fa4d262ee0a69946
Amos Bastian <amosbastian@gmail.com>
2023-05-09 19:13:02 +0200

    feat: add generateStaticParams and generateMetadata to pokemon page

--- apps/web/app/pokemon/[id]/page.tsx
+++ apps/web/app/pokemon/[id]/page.tsx
@@ -1,10 +1,67 @@
+import { BASE_URL } from "@pokemon/configuration";
+import { getAllPokemon, getSinglePokemon } from "@pokemon/db";
 import { Pokemon } from "@pokemon/ui/server";
+import { Metadata } from "next";
 
-export default async function Page({ params }: { params: { id: number } }) {
+type PageProps = {
+  params: {
+    id: number;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
   return (
     <div className="mx-auto flex h-full flex-1 items-center justify-center p-6 sm:min-w-[425px] sm:max-w-[425px]">
       {/* @ts-expect-error Server component */}
       <Pokemon id={params.id} />
     </div>
   );
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const ogUrl = new URL(`${BASE_URL}/api/og`);
+
+  const { pokemon, types } = getSinglePokemon(params.id) ?? {};
+
+  if (!pokemon || !types) {
+    return {};
+  }
+
+  const title = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const description = `HP: ${pokemon.hp}. Attack: ${pokemon.attack}. Defense: ${pokemon.defense}. Special attack: ${pokemon.specialAttack}. Special defense: ${pokemon.specialDefense}. Speed: ${pokemon.speed}.`;
+
+  ogUrl.searchParams.set("pokemon", title);
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `${BASE_URL}/pokemon/${params.id}`,
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: pokemon.name,
+        },
+      ],
+      tags: types.map((type) => type.name),
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogUrl.toString()],
+    },
+  };
+}
+
+export async function generateStaticParams(): Promise<PageProps["params"][]> {
+  const pokemon = getAllPokemon();
+
+  return pokemon.map(({ pokemon: { id } }) => ({
+    id,
+  }));
 }
