8476cf75906baf1c9fede2f68c787757f4d41c81
Amos Bastian <amosbastian@gmail.com>
2023-05-09 22:36:43 +0200

    feat: add header component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,6 @@
 import { BASE_URL, BRAND_DESCRIPTION, BRAND_NAME } from "@pokemon/configuration";
-import { ButtonLink, PokeBalls, ThemeProvider, ThemeToggle } from "@pokemon/ui";
-import { Home } from "lucide-react";
+import { PokeBalls, ThemeProvider } from "@pokemon/ui";
+import { Header } from "@pokemon/ui/server";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles.css";
@@ -24,24 +24,8 @@ export default function RootLayout({ children, modal }: { children: React.ReactN
       <body className="h-full">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="dark:bg-gray-1 h-full bg-white">
-            <header className="absolute inset-x-0 top-0 z-50 py-5">
-              <nav className="mx-auto flex max-w-2xl items-center justify-between gap-x-6" aria-label="Global">
-                <div className="flex lg:flex-1">
-                  <ButtonLink
-                    href="/"
-                    variant="ghost"
-                    className="group flex h-7 w-7 items-center justify-center rounded-md p-0 transition"
-                    aria-label="Go home"
-                  >
-                    <Home className="text-slate-11 group-hover:text-slate-12 absolute h-4 w-4" />
-                  </ButtonLink>
-                </div>
-
-                <div className="flex flex-1 items-center justify-end gap-x-6">
-                  <ThemeToggle />
-                </div>
-              </nav>
-            </header>
+            {/* @ts-expect-error Server component */}
+            <Header />
             <main className="mx-auto flex h-full max-w-2xl flex-col gap-4 pb-4 pt-16">
               {modal}
               {children}
---
+++ libs/ui/src/lib/header/header.tsx
@@ -0,0 +1,31 @@
+import { nextAuthOptions } from "@pokemon/configuration/server";
+import { HomeIcon } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { ButtonLink } from "../button/button";
+import { ThemeToggle } from "../theme-toggle/theme-toggle";
+
+export async function Header() {
+  const session = await getServerSession(nextAuthOptions);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50 py-5">
+      <nav className="mx-auto flex max-w-2xl items-center justify-between gap-x-6" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <ButtonLink
+            href="/"
+            variant="ghost"
+            className="group flex h-7 w-7 items-center justify-center rounded-md p-0 transition"
+            aria-label="Go home"
+          >
+            <HomeIcon className="text-slate-11 group-hover:text-slate-12 absolute h-4 w-4" />
+          </ButtonLink>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <ThemeToggle />
+          {session ? null : <ButtonLink href="/sign-in"></ButtonLink>}
+        </div>
+      </nav>
+    </header>
+  );
+}
